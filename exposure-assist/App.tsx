e3eb0f802e4c2d139cbc64194e0ac0eae3a90dc8
Rob Tron <bobbotron@gmail.com>
2023-08-06 08:28:00 -0400

    Added basic exposure calculator

--- exposure-assist/App.tsx
+++ exposure-assist/App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! Hello.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ExposureCalculator from "./components/ExposureCalculator";
+import { PaperProvider } from "react-native-paper";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 16,
+    paddingBottom: 10,
   },
 });
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text style={styles.header}>Exposure Buddy</Text>
+        <ExposureCalculator />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+  );
+}
